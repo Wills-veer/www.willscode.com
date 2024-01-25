05938e19273cc447b8f7c5e66dd151763a8a8464
Wills-veer <155957223+Wills-veer@users.noreply.github.com>
2024-01-25 10:15:40 +0530

    Add files via upload

---
+++ imp.js
@@ -0,0 +1,73 @@
+export default const quiz = {
+    q1:"How many faces does a Dodecahedron have?",
+    options1: {
+    opt1:"10",
+    opt2:"12",
+    opt3:"14",
+    opt4:"16"
+    },
+    q2:"What game studio makes the Red Dead Redemption series?",
+    options2: {
+    opt1:"Rockstar games",
+    opt2:"Electronic art",
+    opt3:"Epic games",
+    opt4:"Gameloft"
+    },
+    q3:"What city is known as \"The Eternal City\"?",
+    options3: {
+    opt1:"Vatican city",
+    opt2:"Italy",
+    opt3:"Venice",
+    opt4:"Rome"
+    },
+    q4:"Which planet has the most moons?",
+    options4: {
+    opt1:"Jupiter",
+    opt2:"Mars",
+    opt3:"Nepturn",
+    opt4:"Saturn"
+    },
+    q5:"How many bones do we have in an ear?",
+    options5: {
+    opt1:"5",
+    opt2:"4",
+    opt3:"3",
+    opt4:"6"
+    },
+    q6:"How many minutes are there in a full week?",
+    options6: {
+    opt1:"12,080",
+    opt2:"10,080",
+    opt3:"16,080",
+    opt4:"20,280"
+    },
+    q7:"How many verified elements are in the periodic table?",
+    options7: {
+    opt1:"121",
+    opt2:"120",
+    opt3:"118",
+    opt4:"119"
+    },
+    q8:"Aureolin is a shade of what color?",
+    options8: {
+    opt1:"Yellow",
+    opt2:"Blue",
+    opt3:"Violet",
+    opt4:"Red"
+    }, 
+    q9:"Who is credited with the discovery of the periodic table of elements?",
+    options9: {
+    opt1:"John Newlands",
+    opt2:"Vasily Mendeleev",
+    opt3:"John Dalton",
+    opt4:"Dmitri Mendeleev"
+    }, 
+    q10:"Which of the following is a famous example of a gravitational singularity?",
+    options10: {
+    opt1:"The center of our solar system",
+    opt2:"The center of our Milky Way galaxy",
+    opt3:"The Crab Nebula",
+    opt4:"The Great Red Spot on Jupiter"
+    },
+}
+export let crtAns1=quiz.options1.opt2,crtAns2=quiz.options2.opt1,crtAns3=quiz.options3.opt4,crtAns4=quiz.options4.opt4,crtAns5=quiz.options5.opt3,crtAns6=quiz.options6.opt2,crtAns7=quiz.options7.opt3,crtAns8=quiz.options8.opt1,crtAns9=quiz.options9.opt4,crtAns10=quiz.options10.opt2;
